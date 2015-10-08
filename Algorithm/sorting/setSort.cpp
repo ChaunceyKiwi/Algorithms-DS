a536c2acfb629d8977fd48195776622025f8533d
Chauncey <Chaunceykiwi@gmail.com>
2015-10-07 23:14:59 -0700

    add a Readme

---
+++ Algorithm/sorting/setSort.cpp
@@ -0,0 +1,48 @@
+//
+//  main.cpp
+//  homework307
+//
+//  Created by Chauncey on 10/5/15.
+//  Copyright (c) 2015 Chauncey. All rights reserved.
+//
+
+#include <iostream>
+
+
+
+void setSort(int* arr,int n)
+{
+    int count0 = 0;
+    int count1 = 0;
+    int count2 = 0;
+
+    for (int a = 0 ; a < n; a++) {
+        if(arr[a] == 0)
+            count0++;
+        else if (arr[a] == 1)
+            count1++;
+        else if (arr[a] == 2)
+            count2++;
+    }
+    
+    for(int i = 0;i < count0;i++)
+        arr[i] = 0;
+    
+    for(int i = 0;i < count1;i++)
+        arr[count0 + i] = 1;
+    
+    for(int i = 0;i < count2;i++)
+        arr[count0 + count1 + i] = 2;
+}
+
+
+
+int main(void) {
+    int a[12] = {0,0,1,0,1,2,2,1,0,2,1,2};
+    
+    setSort(a,12);
+    
+    return 0;
+}
+
+
