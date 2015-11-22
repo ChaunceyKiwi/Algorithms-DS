0dae0ac38c1905d88e252787e62be35138eedde2
Chauncey <chaunceykiwi@gmail.com>
2015-11-21 23:10:36 -0800

    Add class city

--- Algorithm/MinimumSpanningTree/main.cpp
+++ Algorithm/MinimumSpanningTree/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 using namespace std;
 
+class City{
+private:
+  string* citys;
+  int size;
+
+public:
+  City(int n){
+    citys = new string[n];
+    size = n;
+  }
+
+  ~City(){
+    delete[] citys;
+  }
+
+  void setName(int index,string name){
+    citys[index] = name;
+  }
+
+  string getName(int index){
+    return citys[index];
+  }
+
+  int getIndex(string name){
+    for(int i = 0;i < size;i++){
+      if(citys[i] == name){
+        return i;
+      }
+    }
+    return -1;
+  }
+};
+
 int main(void){
   int m,n,i;
 
