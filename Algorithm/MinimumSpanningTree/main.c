8513c6962a5c0632df21b2fa5a1f636f3a759a3c
Chauncey <chaunceykiwi@gmail.com>
2015-11-21 23:02:58 -0800

    Change to c++ version

--- Algorithm/MinimumSpanningTree/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(void){
-  int m,n;
-  printf("Input m and n:\n");
-  scanf("%d",&m);
-  scanf("%d",&n);
-  printf("m is %d,n is %d\n",m,n);
-}
---
+++ Algorithm/MinimumSpanningTree/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+  int m,n,i;
+
+  cout<<"Input n and m:"<<endl;
+  cin>>n>>m;
+  cout<<"M:"<<m<<" N:"<<n<<endl;
+
+  // for(i = 0;i < m,i++)
+  // scanf("%s\n", );
+
+
+}
