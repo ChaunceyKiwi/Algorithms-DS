17f89909bad2a6c50a782b91eeaaeeef4f7b7bd4
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-01-22 10:26:29 -0800

    update knapsack algorithm, add no recursive version

--- Algorithm/DynamicProgramming/main.cpp
+++ Algorithm/DynamicProgramming/main.cpp
@@ -1,27 +1,65 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int max(int a, int b) { return (a > b)? a : b; }
-
-int knapSack(int W, int wt[], int val[], int n)
+// Recursive version
+int knapSack(int limit, int *weight, int *value, int n)
 {
+  // If there is no item or limit is 0
+  if (n == 0 || limit == 0) {
+    return 0;
+  }
 
-   if (n == 0 || W == 0)
-       return 0;
+  // If the last item is not heavy to carry
+  if (weight[n-1] > limit) {
+    return knapSack(limit, weight, value, n - 1);
+  }
 
-   if (wt[n-1] > W)
-       return knapSack(W, wt, val, n-1);
+  else {
+    // If carry last item
+    int caseA = value[n - 1] +
+      knapSack(limit-weight[n - 1], weight, value, n - 1);
 
-   else return max( val[n-1] + knapSack(W-wt[n-1], wt, val, n-1),
-                    knapSack(W, wt, val, n-1)
-                  );
+    // If not carry last item
+    int caseB = knapSack(limit, weight, value, n - 1);
+
+    return max(caseA, caseB);
+  }
+}
+
+// Non-recursive version
+int knapSack2(int limit, int *weight, int *value, int n)
+{
+  // Initilization
+  int table[n+1][limit+1];
+  for (int i = 0; i < n + 1; i++) {
+    for (int j = 0; j < limit + 1; j++) {
+      table[i][j] = 0;
+    }
+  }
+
+  // Build table
+  for (int i = 1; i < n + 1; i++) {
+    for (int j = 0; j < limit + 1; j++) {
+      if (j - weight[i-1] < 0) {
+        table[i][j] = table[i-1][j];
+      } else {
+        int caseA = table[i-1][j];
+        int caseB = table[i-1][j-weight[i-1]] + value[i-1];
+        table[i][j] = max(caseA, caseB);
+      }
+    }
+  }
+
+  return table[n][limit];
 }
 
 int main()
 {
-    int val[] = {60, 100, 120, 200};
-    int wt[] = {10, 20, 30, 5};
-    int  W = 50;
-    int n = sizeof(val)/sizeof(val[0]);
-    printf("%d\n", knapSack(W, wt, val, n));
-    return 0;
+  int value[4] = {60, 100, 120, 200};
+  int weight[4] = {10, 20, 30, 5};
+  int limit = 50;
+  int n = sizeof(value)/sizeof(value[0]);
+  cout << knapSack(limit, weight, value, n) << endl;;
+  cout << knapSack2(limit, weight, value, n) << endl;
+  return 0;
 }
