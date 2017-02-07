0eac7812d9fbd85312d6db9116e380c76bbce37c
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-02-06 22:56:18 -0800

    file system naming polished

--- algorithm/other/findSum.cpp
+++ algorithm/other/findSum.cpp
@@ -1,28 +1,19 @@
-//
-//  main.cpp
-//  homework307
-//
-//  Created by Chauncey on 10/5/15.
-//  Copyright (c) 2015 Chauncey. All rights reserved.
-//
-
 #include <iostream>
 #include "quickSort.h"
-
 
 //Time complexity = O(n) + 2 * O(nlogn) + o(2n) = O(nlogn)
 bool existSum(int* s1 ,int* s2,int n,int x)
 {
     int s3[8];    //s3[i] = x - s2[i]
-    
+
     for(int k = 0;k < 8;k++)   //give value to s3 ,O(n)
         s3[k] = x - s2[k];
-    
+
     quickSort(s1, 0, n - 1);   //sort array s1, O(nlogn)
     quickSort(s3, 0, n - 1);   //sort array s3, O(nlogn)
 
     int i = 0,j = 0;
-    
+
     //if s1[i] = s3[j],which means s1[i] = x - s2[j],then s1[i] +s2[j] = x
     //Operation need O(2n)
     while(i < n && j < n){
@@ -33,18 +24,13 @@ bool existSum(int* s1 ,int* s2,int n,int x)
     return 0;  //do not exists such a pair
 }
 
-
-
-
 int main(void) {
     int s1[8] = {3,6,8,2,11,4,1,7};
     int s2[8] = {1,8,3,6,23,12,6,9};
-    
+
     if(existSum(s1, s2, 8, 23) == true)
         printf("exists.\n");
     else
         printf("Do not exists.\n");
     return 0;
 }
-
-
