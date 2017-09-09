d3e6314589d635ece313e702bfec9e97025022ad
Chauncey <chaunceykiwi@gmail.com>
2017-09-08 18:22:09 -0700

    Update bubbleSort.h

--- algorithms/sorting/bubbleSort.h
+++ algorithms/sorting/bubbleSort.h
@@ -1,33 +1,31 @@
-// Sorting algorithm with Data structure Array
-// Worst case performance	O(n^2)
-// Best case performance	O(n)
-// Average case performance	O(n^2)
-// Worst case space complexity	O(1) auxiliary
+/* 
+ * Sorting algorithm with Data structure Array
+ * Worst case performance O(n^2)
+ * Best case performance  O(n)
+ * Average case performance O(n^2)
+ * Worst case space complexity  O(1) auxiliary
+ */
 
-//Chauncey:Each iteration the maxmium will be put at the rear of array
-//Some number will bubble up until meet a larger number
-
+/* Each iteration the maxmium will be put at the rear of array */
+/* Some number will bubble up until meet a larger number */
 
 #include <stdio.h>
-
 void bubbleSort(int* arr,int n)
 {
-    bool swapped = true; //when array is in order now,swapped will be false and sorting terminate in advance.
-    int j = 0;
-    int tmp;
-    while(swapped)
-    {
-        swapped = false;
-        j++;
-        for(int i = 0;i < n- j;i++)
-        {
-            if(arr[i] > arr[i+1])
-            {
-                tmp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = tmp;
-                swapped = true;
-            }
-        }
+  bool swapped = true; //when array is in order now,swapped will be false and sorting terminate in advance.
+  int j = 0;
+  int tmp;
+  while (swapped) {
+    swapped = false;
+    j++;
+    
+    for (int i = 0;i < n - j;i++) {
+      if (arr[i] > arr[i+1]) {
+        tmp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = tmp;
+        swapped = true;
+      }
     }
+  }
 }
